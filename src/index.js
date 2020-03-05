c384e8b0c1e1d3eac45e6e717299c46a4b36b174
Egor Dubovik <Hamelion74@gmail.com>
2020-03-05 20:57:58 +0300

    feat: add array functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 exports.min = function min (array) {
-   return 0;
+  if (!array || array.length === 0) { return 0; }
+  var min_v = array[0];
+  for (var i = 0; i < array.length; i++) {
+      if (min_v > array[i]) min_v = array[i];
+  }
+  return min_v;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) { return 0; }
+  var max_v = array[0];
+  for (var i = 0; i < array.length; i++) { 
+      if (max_v < array[i]) max_v = array[i]; 
+  }
+  return max_v;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) { return 0; }
+  var avg_v =  0;
+  for (var i = 0; i < array.length; i++) { 
+    avg_v += array[i];
+  }
+
+  return avg_v == 0 ? avg_v : avg_v / array.length;
 }
